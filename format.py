059d49da2eefab7286bf4367e3805c2f90a49780
ad546 <ad546github@gmail.com>
2018-12-09 16:45:12 -0600

    put everything into functions

--- format.py
+++ format.py
@@ -2,28 +2,32 @@
 import openpyxl
 import os
 
+stringMatchers = ["started", "Survey"]
+
 def openWorkbook():
     wbName = '23630_ROTATION'
     #input("What is the name of your workbook?")
     wb = openpyxl.load_workbook(wbName + '.xlsx')
     sheet = wb.get_sheet_by_name(wbName)
-    workbookData = []
-    stringMatchers = ["started", "Survey"]
-    userID = sheet['B3'].value
+    return sheet
 
+def readFiles(sheet):
+    workbookData = []
+    userID = sheet['B3'].value
     print("Reading rows...")
     for row in range(5, sheet.max_row + 1):
         message = sheet['C' + str(row)].value
         if any(x in message for x in stringMatchers):
             workbookData.append(message)
-        '''if "started" in sheet['C' + str(row)].value:
-            message = sheet['C' + str(row)].value
-            workbookData.append(message)
-        elif "Survey answers" in sheet['C' + str(row)].value:
-            message = sheet['C' + str(row)].value
-            workbookData.append(message)'''
+    return workbookData, userID
 
+def printData(workbookData, userID):
     print("Here is your data for " + str(userID))
     print(workbookData)
 
-openWorkbook()
+def main():
+    workingSheet = openWorkbook()
+    workbookData, userID = readFiles(workingSheet)
+    printData(workbookData, userID)
+
+main()
