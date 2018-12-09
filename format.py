23c8683dadabffd1ec4eaf627e51f38fa502d307
ad546 <ad546github@gmail.com>
2018-12-09 16:29:01 -0600

    implemented data cleaning

--- format.py
+++ format.py
@@ -8,12 +8,20 @@ def openWorkbook():
     wb = openpyxl.load_workbook(wbName + '.xlsx')
     sheet = wb.get_sheet_by_name(wbName)
     workbookData = []
+    stringMatchers = ["started", "Survey"]
     userID = sheet['B3'].value
 
     print("Reading rows...")
     for row in range(5, sheet.max_row + 1):
         message = sheet['C' + str(row)].value
-        workbookData.append(message)
+        if any(x in message for x in stringMatchers):
+            workbookData.append(message)
+        '''if "started" in sheet['C' + str(row)].value:
+            message = sheet['C' + str(row)].value
+            workbookData.append(message)
+        elif "Survey answers" in sheet['C' + str(row)].value:
+            message = sheet['C' + str(row)].value
+            workbookData.append(message)'''
 
     print("Here is your data for " + str(userID))
     print(workbookData)
