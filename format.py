ea88c3f7a6e4bfae745b2cf1cc93e90d86272104
ad546 <ad546github@gmail.com>
2018-12-09 19:33:44 -0600

    version for PC files

--- format.py
+++ format.py
@@ -10,10 +10,13 @@ def runScript():
     wbName = '23630_ROTATION'
     wb = openpyxl.load_workbook(wbName + '.xlsx')
     sheet = wb[wbName]
+    platform = sheet['B2'].value
     userID = sheet['B3'].value
     
     #Read data from workbook
     print("Reading rows...")
+    holdingCell = []
+    surveyCounter = 0
     for row in range(5, sheet.max_row + 1):
         message = sheet['C' + str(row)].value
         if not any(y in message for y in forbiddenWords):
@@ -24,11 +27,12 @@ def runScript():
     wb2 = openpyxl.load_workbook('testResults.xlsx')
     sheet2 = wb2['Survey Results']
     for index, data in enumerate(workbookData, start=1):
-        sheet2['A' + str(index)] = userID
-        sheet2['B' + str(index)] = data
+        sheet2['A' + str(index)] = platform
+        sheet2['B' + str(index)] = userID
+        sheet2['C' + str(index)] = data
 
     wb2.save('testResults.xlsx')
-    print("DONE!")
+    print('DONE!')
 
 runScript()
 
