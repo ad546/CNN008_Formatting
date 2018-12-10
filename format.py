1bb59191f28eb7d0a43e483c1dba3b4a43000968
ad546 <ad546github@gmail.com>
2018-12-09 22:21:05 -0600

    broken, need to link messages with IDs

--- format.py
+++ format.py
@@ -1,35 +1,50 @@
 #This program is to format
+#Script will work with PC files
 import openpyxl
 import os
+
+'''def loopFiles():
+    dirs = os.listdir('./')
+    for dir in dirs:
+        if dir.endswith(".xlsx"):
+            if dir != 'testResults.xlsx':
+                print("Running")
+                runScript(dir)'''
 
 def runScript():
     stringMatchers = ['started', 'Survey', 'selected']
     forbiddenWords = ['test_ads', 'selected undefined']
     workbookData = []
-    #Open Workbook
-    wbName = '23630_ROTATION'
-    wb = openpyxl.load_workbook(wbName + '.xlsx')
-    sheet = wb[wbName]
+    wbName = 'combine.xlsx'
+    wb = openpyxl.load_workbook(wbName)
+    sheet = wb.active
     platform = sheet['B2'].value
     userID = sheet['B3'].value
     
     #Read data from workbook
     print("Reading rows...")
-    holdingCell = []
-    surveyCounter = 0
     for row in range(5, sheet.max_row + 1):
-        message = sheet['C' + str(row)].value
+        if sheet['A' + str(row)].value == 'Member ID':
+            userID = sheet['B' + str(row)].value
+
+        if sheet['C' + str(row)].value == None:
+            message = "blank"
+        else:
+            message = sheet['C' + str(row)].value
+
         if not any(y in message for y in forbiddenWords):
             if any(x in message for x in stringMatchers):
                 workbookData.append(message)
-
+                #need to link messages to ID
     #Save data in workbook
     wb2 = openpyxl.load_workbook('testResults.xlsx')
     sheet2 = wb2['Survey Results']
+    row_count = sheet2.max_row
+    print(row_count)
     for index, data in enumerate(workbookData, start=1):
-        sheet2['A' + str(index)] = platform
-        sheet2['B' + str(index)] = userID
-        sheet2['C' + str(index)] = data
+        sheet2['A' + str(index+row_count)] = platform
+        sheet2['B' + str(index+row_count)] = userID
+        sheet2['C' + str(index+row_count)] = data
 
     wb2.save('testResults.xlsx')
     print('DONE!')
