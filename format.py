7044a7a2a4d79e6834ee8f31919275e5e21fc8cc
Austin Daspit <adaspit@corp.mediasciencelabs.com>
2018-12-13 11:23:09 -0600

    first phase working

--- format.py
+++ format.py
@@ -1,6 +1,7 @@
 #This program is to format
 #Script will work with PC files
 #Something is wrong with how the log files are saved. Have to open in excel and save as csv
+#Todo: group results based on stories / further clean selected, started / fix null byte issue
 import openpyxl
 import os
 import csv
@@ -8,30 +9,37 @@ import glob
 
 def loopFiles():
     path = "./*.csv"
-    line = 0
     for fname in glob.glob(path):
-        line += 1
-        print(line)
         runScript(fname)
 
 def runScript(fname):
     cleanData = []
     stringMatchers = ['started', 'Survey', 'selected']
     forbiddenWords = ['test_ads', 'selected undefined']
-    with open(fname, newline='') as csv_file:
-        dataArray = list(csv.reader(csv_file))
+    print(fname)
+    if fname != '.\Book1.csv':
+        with open(fname, newline='') as csv_file:
+            dataArray = list(csv.reader(csv_file))
 
-    for i in range(len(dataArray)):
-        if not any(y in dataArray[i][2] for y in forbiddenWords):
-            if any(x in dataArray[i][2] for x in stringMatchers):
-                terribleList = []
-                terribleList.append(dataArray[i][1])
-                terribleList.append(dataArray[i][2])
-                cleanData.append(terribleList)
+        for i in range(len(dataArray)):
+            if not any(y in dataArray[i][2] for y in forbiddenWords):
+                if any(x in dataArray[i][2] for x in stringMatchers):
+                    terribleList = []
+                    terribleList.append(dataArray[i][1])
+                    terribleList.append(dataArray[i][2])
+                    cleanData.append(terribleList)
 
-    for data in cleanData:
-        print(data)
-        print('\n')
+        writetoFile(cleanData)
 
-loopFiles()
+def writetoFile(datatoWrite):
+    #Book1
+    with open('Book1.csv', 'a') as f:
+        writer = csv.writer(f, delimiter = ',', lineterminator='\n')
+        for value in datatoWrite:
+            print(value)
+            writer.writerow(value)
 
+def main():
+    loopFiles()
+
+main()
